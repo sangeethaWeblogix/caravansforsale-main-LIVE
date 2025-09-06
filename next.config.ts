e5481a87db5d130ddef2feb6e0691b70fdad4b02
pavi <pavi.s@weblogix.com.au>
2025-09-06 16:27:38 +0530

    env chnges

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true, // bypass Next.js optimization
-    remotePatterns: [
-      { protocol: "https", hostname: "**.caravansforsale.com.au" },
-      { protocol: "https", hostname: "admin.caravansforsale.com.au" },
-    ],
   },
 };
 
