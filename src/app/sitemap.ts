9a7cce36dee74f40b59c99157ebf0b6218605d52
pavi <pavi.s@weblogix.com.au>
2025-09-10 11:25:28 +0530

    sitemap check

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,45 +14,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${siteUrl}/blogs-sitemap.xml`,
       lastModified: new Date(),
     },
-    {
-      url: `${siteUrl}/listigs-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/makes-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/states-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/categories-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/regions-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/prices-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/weights-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/sleeps-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/length-sitemap.xml`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${siteUrl}/conditions-sitemap.xml`,
-      lastModified: new Date(),
-    },
   ];
 }
