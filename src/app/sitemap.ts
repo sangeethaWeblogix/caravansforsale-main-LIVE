231b1e0051ce8bd989897812b84561248c229e36
pavi <pavi.s@weblogix.com.au>
2025-09-18 17:10:49 +0530

    lisiting chnages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,7 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
     {
-      url: `${siteUrl}/listigs-sitemap.xml`,
+      url: `${siteUrl}/listings-sitemap.xml`,
       lastModified: new Date(),
     },
     {
