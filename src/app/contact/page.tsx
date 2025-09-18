c044753aa225e3cd0abc1e73c1f019f785661c71
pavi <pavi.s@weblogix.com.au>
2025-09-18 19:33:12 +0530

    build error fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,31 +1,6 @@
 import Contacts from "./contacts";
 import "./contact.css";
 import { Metadata } from "next";
-export async function generateMetadata(): Promise<Metadata> {
-  const metaTitle = "Contact Off Road Caravans - offroadcaravans.com";
-  const metaDescription =
-    "Contact offroadcaravans.com Australia . Fill in our contact form if you have any questions related to buying an off road caravan & we will get back to you.";
-
-  const robots = "index, follow";
-
-  return {
-    title: metaTitle,
-    description: metaDescription,
-    robots: robots,
-    openGraph: {
-      title: metaTitle,
-      description: metaDescription,
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: metaTitle,
-      description: metaDescription,
-    },
-    alternates: {
-      canonical: "https://www.caravansforsale.com.au/contact/", // ✅ canonical link
-    },
-  };
-}
 
 export const metadata: Metadata = {
   title: "Contact Off Road Caravans - offroadcaravans.com",
