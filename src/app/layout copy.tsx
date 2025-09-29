0638f0e81294e651f68b0ff9326b719b6c490ce7
pavi <pavi.s@weblogix.com.au>
2025-09-29 18:16:56 +0530

    gtag added

--- src/app/layout copy.tsx
+++ src/app/layout copy.tsx
@@ -7,7 +7,6 @@ import Footer from "./footer/Footer";
 import React from "react";
 import { Metadata } from "next";
 import ScrollToTop from "./ScrollToTop";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -46,19 +45,19 @@ export default function RootLayout({
         />
 
         {/* ✅ Google Tag Manager (Head) */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-86MYWLZRTY"
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-56V5NDV');
+            `,
+          }}
         />
-
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-86MYWLZRTY');
-          `}
-        </Script>
       </head>
       <body
         className="flex flex-col min-h-screen new_font"
