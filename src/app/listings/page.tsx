d2ecef10897ad7f1d1856e67ea8e50986de07733
pavi <pavi.s@weblogix.com.au>
2025-11-14 19:40:10 +0530

    build error check

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,4 +1,4 @@
- import React, { Suspense } from "react";
+ import React from "react";
   import type { Metadata } from "next";
 import { generateListingsMetadata } from "@/utils/seo/listingsMetadata";
  import "../components/ListContent/newList.css"
