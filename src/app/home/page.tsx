557fd1104e1f570eaaaa39ae64b9034c45e7787f
pavi <pavi.s@weblogix.com.au>
2025-12-09 17:18:05 +0530

    home changes  - meta

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,30 +2,27 @@ import type { Metadata } from "next";
 import Home from "./home";
 import "../globals.css";
 
-export const metadata: Metadata = {
+ export const metadata: Metadata = {
   title: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
   description:
     "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
   robots: "index, follow",
-  verification: {
-    google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ this auto generates <meta name="google-site-verification" />
-  },
   openGraph: {
-         title: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
-
-    description:
-      "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
+   title: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
+  description:
+    "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
   },
-
   twitter: {
     card: "summary_large_image",
-       title: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
-
-    description:
-      "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
+     title: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
+  description:
+    "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
   },
   alternates: {
-    canonical: "https://www.caravansforsale.com.au/",
+    canonical: "https://www.caravansforsale.com.au",
+  },
+  verification: {
+    google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ add here
   },
 };
 
