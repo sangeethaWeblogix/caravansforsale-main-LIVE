0769f8e64259f83b5da16b0a84f6f314f3874a8f
pavi <pavi.s@weblogix.com.au>
2025-12-15 20:42:28 +0530

    home page changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,15 @@ export const metadata: Metadata = {
   description:
     "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity",
   icons: { icon: "/favicon.ico" },
+  robots: "index, follow",
   verification: {
     google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ this auto generates <meta name="google-site-verification" />
   },
   alternates: {
-    canonical: "https://www.caravansforsale.com.au/", // ✅ base canonical
+    canonical: "https://www.caravansforsale.com.au",
   },
+  
+
 };
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
 import type { Metadata } from "next";
 import Home from "./home/home";
 import "./globals.css";
- export const metadata: Metadata = {
-  title: "Caravans For Sale – Australia’s Marketplace for New & Used Caravans",
-  description:
-    "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
-  robots: "index, follow",
-  openGraph: {
-   title: "Caravans For Sale – Australia’s Marketplace for New & Used Caravans",
-  description:
-    "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
-  },
-  twitter: {
-    card: "summary_large_image",
-     title: "Caravans For Sale – Australia’s Marketplace for New & Used Caravans",
-  description:
-    "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity.",
-  },
-  alternates: {
-    canonical: "https://www.caravansforsale.com.au",
-  },
-  verification: {
-    google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ add here
-  },
-};
+ 
 
 const Page = () => (
   <div>
