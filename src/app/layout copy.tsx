46431622f5b3841ee1f9b9600c68e44aeb8a1607
pavi <pavi.s@weblogix.com.au>
2025-12-16 10:46:36 +0530

    changes

--- src/app/layout copy.tsx
+++ src/app/layout copy.tsx
@@ -1,4 +1,4 @@
- export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -13,25 +13,31 @@ import UTMTracker from "./UTMTracker";
 
 export const metadata: Metadata = {
   title: {
-    default: "Caravans For Sale – #1 Marketplace for New & Used Caravan Deals",
+    default: "Caravans For Sale – Australia’s Marketplace for New & Used Caravans",
     template: "%s ",
   },
   description:
     "Browse new & used caravans for sale across Australia. Compare prices on off-road, hybrid, pop top, touring, luxury models with size, weight & sleeping capacity",
   icons: { icon: "/favicon.ico" },
+  robots: "index, follow",
   verification: {
     google: "6tT6MT6AJgGromLaqvdnyyDQouJXq0VHS-7HC194xEo", // ✅ this auto generates <meta name="google-site-verification" />
   },
   alternates: {
-    canonical: "https://www.caravansforsale.com.au/", // ✅ base canonical
+    canonical: "https://www.caravansforsale.com.au",
   },
+  
+
 };
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const gtmServer = process.env.NEXT_PUBLIC_GTM_SERVER_URL;
   return (
     <html lang="en">
       <head>
@@ -48,26 +54,26 @@ export default function RootLayout({
         />
 
         {/* ✅ Google Tag Manager (Head) */}
-       
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-86MYWLZRTY"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true; 
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N3362FGQ');
-            `,
-          }}
-        />
+         <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+
+                  '&gtm_url=${gtmServer}';
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `,
+      }}
+    />
+      
+        
       </head>
       <body
         className="flex flex-col min-h-screen new_font"
@@ -77,14 +83,14 @@ export default function RootLayout({
         }}
       >
         {/* ✅ Google Tag Manager (noscript) - right after body */}
-           <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N3362FGQ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+          <noscript>
+    <iframe
+      src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
 
         <UTMTracker />
         <Navbar />
