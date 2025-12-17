277b3d87b6bd5dc9b98a1db5b1ec4b271f3b9c18
pavi <pavi.s@weblogix.com.au>
2025-12-17 09:22:46 +0530

    blog changes canoical

--- src/app/(blogs)/blog/page.tsx
+++ src/app/(blogs)/blog/page.tsx
@@ -8,6 +8,10 @@ export const metadata: Metadata = {
     "Latest news, in-depth reviews, and expert advice on the latest in the caravan market. Stay informed and make smarter decisions.",
   icons: { icon: "/favicon.ico" },
   robots: "index, follow",
+  alternates: {
+    canonical: "https://www.caravansforsale.com.au/blog/",
+
+   },
 };
 
 export const revalidate = 60; // ISR: refresh every 60s
