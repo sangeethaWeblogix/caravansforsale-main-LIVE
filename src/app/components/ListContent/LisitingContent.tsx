cc9d145ac6f4953943537f2287133e277088745f
pavi <pavi.s@weblogix.com.au>
2026-01-10 18:36:56 +0530

    sample image revert

--- src/app/components/ListContent/LisitingContent.tsx
+++ src/app/components/ListContent/LisitingContent.tsx
@@ -207,14 +207,14 @@ export default function ListingContent({
   };
   const getFirstValidImage = async (item: Product): Promise<string> => {
     const base = getBaseImageUrl(item);
-    if (!base) return "/images/sample3.webp";
+    // if (!base) return "/images/sample3.webp";
 
     for (const ext of IMAGE_FORMATS) {
       const url = `${base}main1.${ext}`;
       if (await checkImage(url)) return url;
     }
 
-    return "/images/sample3.webp";
+    return "";
   };
 
   const MAX_SWIPER_IMAGES = 5;
@@ -256,9 +256,9 @@ const loadRemaining = async (item: Product) => {
   }
 
   // ❗ If only main1 exists, still safe
-  if (validImages.length === 0) {
-    validImages.push("/images/sample3.webp");
-  }
+  // if (validImages.length === 0) {
+  //   validImages.push("/images/sample3.webp");
+  // }
 
   setLazyImages((prev) => ({
     ...prev,
@@ -602,6 +602,7 @@ useEffect(() => {
                 <span className="show_count mb-2 d-inline">
                   <strong>{count} </strong>
                 </span>
+                
               )}
               <h1 className="show_count d-inline">
                 <strong>{text}</strong>
@@ -677,8 +678,7 @@ useEffect(() => {
                     const isPriority = index < 5;
                     // const resizedBase = getResizedBase(item);
                     const imgs =
-  lazyImages[item.id] ||
-  ["/images/sample3.webp"]; // ALWAYS SAFE
+  lazyImages[item.id] ; // ALWAYS SAFE
 
 
                     return (
