5a3206a1687deadf06b1272cbbd3eabf170781b9
pavi <pavi.s@weblogix.com.au>
2026-01-10 15:38:05 +0530

    change

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -2,6 +2,6 @@ const API_BASE = process.env.NEXT_PUBLIC_CFS_API_BASE;
 
 export function fixImageUrl(url?: string): string {
   if (!url) return "/placeholder.jpg"; // fallback image
-  if (url.startsWith("http")) return url;
+  if (url.startsWith("https")) return url;
   return `${API_BASE}${url}`;
 }
