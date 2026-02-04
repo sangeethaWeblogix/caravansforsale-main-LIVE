740b59f13c74eedfa23e1899c148bea78950a8b9
pavi <pavi.s@weblogix.com.au>
2026-02-04 09:30:06 +0530

    gtag changes old model

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,23 +58,19 @@ const gtmServer = "https://gtm.caravansforsale.com.au";
   
           {/* ✅ Google Tag Manager (Head) */}
            <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+
-                    '&gtm_url=${gtmServer}';
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `,
-        }}
-      />
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-86MYWLZRTY"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-86MYWLZRTY');
+    `,
+          }}
+        />
         
           
         </head>
@@ -87,12 +83,13 @@ const gtmServer = "https://gtm.caravansforsale.com.au";
         >
           {/* ✅ Google Tag Manager (noscript) - right after body */}
             <noscript>
-      <iframe
-        src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-        height="0"
-        width="0"
-        style={{ display: "none", visibility: "hidden" }}
-      />
+     <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-56V5NDV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+          
     </noscript>
   
           <UTMTracker />
