dc112b2d4210a26af19b3c09a69b9734a8e01b78
Moorthi <moorthi.s@weblogix.com.au>
2026-02-25 18:03:38 +0530

    Listing top filter modify

--- src/app/components/ListContent/exculsiveContent.tsx
+++ src/app/components/ListContent/exculsiveContent.tsx
@@ -348,11 +348,11 @@ console.log("exxx", data)
         <meta name="twitter:description" content={metaDescription} />
       </Head>
 
-      <div className="col-lg-6 ">
+      <div className="col-lg-9 ">
         <div className="top-filter mb-10">
           <div className="row align-items-center">
-            <div className="col-lg-8">
-              <h1 className="show_count">
+            <div className="col-lg-12">
+              <h1 className="show_count text-center d-block mb-3">
                 <strong>{pageTitle}</strong>
               </h1>
             </div>
