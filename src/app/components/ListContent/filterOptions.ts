8d48d73250ace3c7a17aa68132a1e1bc9c79c6ed
Prasaanth R <prasaanth@local>
2026-02-26 17:54:48 +0530

    Add filterOptions

---
+++ src/app/components/ListContent/filterOptions.ts
@@ -0,0 +1,110 @@
+export const filterOptions = {
+  categories: [
+    { name: "Off Road", slug: "/off-road-category/" },
+    { name: "Family", slug: "/family-category/" },
+    { name: "Touring", slug: "/touring-category/" },
+    { name: "Luxury", slug: "/luxury-category/" },
+    { name: "Pop Top", slug: "/pop-top-category/" },
+    { name: "Hybrid", slug: "/hybrid-category/" },
+  ],
+  price: [
+    { name: "under 20k", slug: "/under-20000/" },
+    { name: "20k-30k", slug: "/between-20000-30000/" },
+    { name: "30k-40k", slug: "/between-30000-40000/" },
+    { name: "40k-50k", slug: "/between-40000-50000/" },
+    { name: "50k-70k", slug: "/between-50000-70000/" },
+    { name: "70k-100k", slug: "/between-70000-100000/" },
+    { name: "100k-150k", slug: "/between-100000-150000/" },
+    { name: "150k-200k", slug: "/between-150000-200000/" },
+    { name: "over 200k", slug: "/over-200000/" },
+  ],
+  atm: [
+    { name: "Under 1500kg", slug: "/under-1500-kg-atm/" },
+    { name: "1500kg-2500kg", slug: "/between-1500-kg-2500-kg-atm/" },
+    { name: "2500kg-3500kg", slug: "/between-2500-kg-3500-kg-atm/" },
+    { name: "3500kg-4500kg", slug: "/between-3500-kg-4500-kg-atm/" },
+    { name: "Over 4500kg", slug: "/over-4500-kg-atm/" },
+  ],
+  sleep: [
+    { name: "Sleeps 1-2", slug: "/between-1-2-people-sleeping-capacity/" },
+    { name: "Sleeps 3-4", slug: "/between-3-4-people-sleeping-capacity/" },
+    { name: "Sleeps 4-6", slug: "/between-4-6-people-sleeping-capacity/" },
+    { name: "Over 6 Sleeps", slug: "/over-6-people-sleeping-capacity/" },
+  ],
+  length: [
+    { name: "Under 12ft", slug: "/under-12-length-in-feet/" },
+    { name: "12ft-14ft", slug: "/between-12-14-length-in-feet/" },
+    { name: "15ft-17ft", slug: "/between-15-17-length-in-feet/" },
+    { name: "18ft-20ft", slug: "/between-18-20-length-in-feet/" },
+    { name: "21ft-23ft", slug: "/between-21-23-length-in-feet/" },
+    { name: "Over 24ft", slug: "/over-24-length-in-feet/" },
+  ],
+  location: {
+    state: [
+      {
+        name: "Australian Capital Territory",
+        slug: "/australian-capital-territory-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "New South Wales",
+        slug: "/new-south-wales-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "Northern Territory",
+        slug: "/northern-territory-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "Queensland",
+        slug: "/queensland-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "South Australia",
+        slug: "/south-australia-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "Tasmania",
+        slug: "/tasmania-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "Victoria",
+        slug: "/victoria-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+      {
+        name: "Western Australia",
+        slug: "/western-australia-state/",
+        region: [
+          { name: "", slug: "" },
+          { name: "", slug: "" },
+        ],
+      },
+    ],
+  },
+};
