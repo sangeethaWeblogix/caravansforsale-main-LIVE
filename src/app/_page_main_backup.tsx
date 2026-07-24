c17f2fd9592a11b9e1a4935a8d18e0781beeccc1
pavi <pavi@marketplacenetwork.com.au>
2026-07-24 13:46:40 +0530

    check 2

--- src/app/_page_main_backup.tsx
+++ src/app/_page_main_backup.tsx
@@ -1,13 +1,7 @@
- import Home from "./home/home";
+ import Home from "./home-demo/home";
 import "./globals.css?=1";
 import { Metadata } from "next";
-import { fetchSleepBands } from "@/api/homeApi/sleep/api";
-import { fetchRegion } from "@/api/homeApi/region/api";
-import { fetchManufactures } from "@/api/homeApi/manufacture/api";
-import { fetchPriceBasedCaravans } from "@/api/homeApi/price/api";
-import { fetchAtmBasedCaravans } from "@/api/homeApi/weight/api";
-import { fetchLengthBasedCaravans } from "@/api/homeApi/length/api";
-import { fetchUsedCaravansList } from "@/api/homeApi/usedCaravanList/api";
+ 
 import { fetchStateBasedCaravans } from "@/api/homeApi/state/api";
 import { fetchRequirements } from "@/api/postRquirements/api";
 
@@ -93,23 +87,11 @@ const homeJsonLd = {
 
 export default async function Page() {
    const [
-     sleepBands,
-     regionBands,
-     manufactureBands,
-     atmBands,
-     lengthBands,
-     priceBands,
-     usedData,
+     
      stateBands,
       requirements,
    ] = await Promise.all([
-     fetchSleepBands(),
-     fetchRegion(),
-     fetchManufactures(),
-     fetchAtmBasedCaravans(),
-     fetchLengthBasedCaravans(),
-     fetchPriceBasedCaravans(),
-     fetchUsedCaravansList(),
+   
      fetchStateBasedCaravans(),
      fetchRequirements(),
    ]);
@@ -120,13 +102,7 @@ export default async function Page() {
          dangerouslySetInnerHTML={{ __html: JSON.stringify(homeJsonLd) }}
        />
        <Home
-         sleepBands={sleepBands}
-       regionBands={regionBands}
-       manufactureBands={manufactureBands}
-       atmBands={atmBands}
-       lengthBands={lengthBands}
-       priceBands={priceBands}
-       usedData={usedData}
+        
        stateBands={stateBands}
        requirements={requirements}
        />
